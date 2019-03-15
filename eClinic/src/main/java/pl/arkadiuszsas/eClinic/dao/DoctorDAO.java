9af05daac28262a2216fcb54c0aeaf9522fbe9d5
arkadiuszsas <kontakt.arkadiusz.sas@gmail.com>
2019-03-15 14:59:53 +0100

    create DoctorDAO interface and its implementation

---
+++ eClinic/src/main/java/pl/arkadiuszsas/eClinic/dao/DoctorDAO.java
@@ -0,0 +1,10 @@
+package pl.arkadiuszsas.eClinic.dao;
+
+import java.util.List;
+
+import pl.arkadiuszsas.eClinic.entity.Doctor;
+
+public interface DoctorDAO {
+
+	public List<Doctor> getDoctors();	
+}
---
+++ eClinic/src/main/java/pl/arkadiuszsas/eClinic/dao/DoctorDAOImpl.java
@@ -0,0 +1,35 @@
+package pl.arkadiuszsas.eClinic.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import pl.arkadiuszsas.eClinic.entity.Doctor;
+
+@Repository
+public class DoctorDAOImpl implements DoctorDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory; // injected hibernate session factory
+	
+	@Override
+	@Transactional
+	public List<Doctor> getDoctors() {
+		
+		// get the current hibernate session
+		Session currentHibernateSession = sessionFactory.getCurrentSession(); 
+		
+		Query<Doctor> hqlQuery = currentHibernateSession.createQuery("FROM doctor", Doctor.class);
+		
+		// get the result of the query list
+		List<Doctor> allDoctors = hqlQuery.getResultList(); 
+		
+		return allDoctors;
+	}
+
+}
