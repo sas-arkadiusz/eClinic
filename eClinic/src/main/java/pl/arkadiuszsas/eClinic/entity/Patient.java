e42987dfe97c4441f1292ce1608b3147721ca888
arkadiuszsas <kontakt.arkadiusz.sas@gmail.com>
2019-03-16 00:41:06 +0100

    fixed Patient class, fixed OneToMany relationship between Patiend and Doctor

--- eClinic/src/main/java/pl/arkadiuszsas/eClinic/entity/Patient.java
+++ eClinic/src/main/java/pl/arkadiuszsas/eClinic/entity/Patient.java
@@ -1,82 +1,110 @@
 package pl.arkadiuszsas.eClinic.entity;
 
 import java.sql.Date;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-
 @Entity
-@Table(name = "doctor")
-public class Doctor {
+@Table(name = "patient")
+public class Patient {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "doctor_id")
-	private int doctorId;
-	
+	@Column(name = "patient_id")
+	private int patientId;
+
+	@ManyToOne
+	@JoinColumn(name = "doctor_id")
+	private Doctor doctorId;
+
+	@NotNull
+	@Column(name = "file_id", unique = true)
+	private int fileId;
+
 	@NotNull
 	@Size(max = 25)
 	@Column(name = "first_name")
 	private String firstName;
-	
+
 	@NotNull
 	@Size(max = 50)
 	@Column(name = "last_name")
 	private String lastName;
-	
+
 	@NotNull
 	@Column(name = "date_of_birth")
 	private Date dateOfBirth;
-	
+
 	@Size(max = 254)
 	@Column(name = "email_address")
 	private String emailAddress;
-	
+
 	@Size(max = 15)
 	@Column(name = "phone_number")
 	private String phoneNumber;
-	
+
 	@NotNull
 	@Size(max = 50)
 	@Column(name = "street")
 	private String streetName;
-	
+
 	@NotNull
 	@Size(max = 50)
 	@Column(name = "city")
 	private String cityName;
-	
+
 	@NotNull
 	@Size(max = 10)
 	@Column(name = "postal_code")
 	private String postalCode;
-	
+
 	@NotNull
-	@Column(name = "salary")
-	private int salary;
-	
-	@OneToMany(mappedBy = "doctorId")
-	private List<Patient> doctorsPatients;
-	
-	public Doctor() {
-		
+	@Column(name = "first_visit_date")
+	private Date firstVisitDate;
+
+	@NotNull
+	@Column(name = "last_visit_date")
+	private Date lastVisitDate;
+
+	@NotNull
+	@Column(name = "is_insurance")
+	private boolean isInsurance;
+
+	public Patient() {
+
 	}
 
-	public int getDoctorId() {
+	public int getPatientId() {
+		return patientId;
+	}
+
+	public void setPatientId(int patientId) {
+		this.patientId = patientId;
+	}
+
+	public Doctor getDoctorId() {
 		return doctorId;
 	}
 
-	public void setDoctorId(int doctorId) {
+	public void setDoctorId(Doctor doctorId) {
 		this.doctorId = doctorId;
+	}
+
+	public int getFileId() {
+		return fileId;
+	}
+
+	public void setFileId(int fileId) {
+		this.fileId = fileId;
 	}
 
 	public String getFirstName() {
@@ -143,29 +171,37 @@ public class Doctor {
 		this.postalCode = postalCode;
 	}
 
-	public int getSalary() {
-		return salary;
+	public Date getFirstVisitDate() {
+		return firstVisitDate;
 	}
 
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
-	
-	public List<Patient> getDoctorsPatients() {
-		return doctorsPatients;
+	public void setFirstVisitDate(Date firstVisitDate) {
+		this.firstVisitDate = firstVisitDate;
 	}
 
-	public void setDoctorsPatients(List<Patient> doctorsPatients) {
-		this.doctorsPatients = doctorsPatients;
+	public Date getLastVisitDate() {
+		return lastVisitDate;
+	}
+
+	public void setLastVisitDate(Date lastVisitDate) {
+		this.lastVisitDate = lastVisitDate;
+	}
+
+	public boolean isInsurance() {
+		return isInsurance;
+	}
+
+	public void setInsurance(boolean isInsurance) {
+		this.isInsurance = isInsurance;
 	}
 
 	@Override
 	public String toString() {
-		return "Doctor [doctorId=" + doctorId + ", firstName=" + firstName + ", lastName=" + lastName + ", dateOfBirth="
-				+ dateOfBirth + ", emailAddress=" + emailAddress + ", phoneNumber=" + phoneNumber + ", streetName="
-				+ streetName + ", cityName=" + cityName + ", postalCode=" + postalCode + ", salary=" + salary + "]";
+		return "Patient [patientId=" + patientId + ", doctorId=" + doctorId + ", fileId=" + fileId + ", firstName="
+				+ firstName + ", lastName=" + lastName + ", dateOfBirth=" + dateOfBirth + ", emailAddress="
+				+ emailAddress + ", phoneNumber=" + phoneNumber + ", streetName=" + streetName + ", cityName="
+				+ cityName + ", postalCode=" + postalCode + ", firstVisitDate=" + firstVisitDate + ", lastVisitDate="
+				+ lastVisitDate + ", isInsurance=" + isInsurance + "]";
 	}
-	
-	
-	
+
 }
---
+++ eClinic/src/main/webapp/WEB-INF/pages/show-patients.jsp
@@ -0,0 +1,70 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="ISO-8859-1">
+	<title>eClinic</title>
+	<link type="text/css"
+		  rel="stylesheet"
+		  href="${ pageContext.request.contextPath }/resources/css/style.css" >
+</head>
+
+<body>
+
+	<div class="container">
+
+		<div class="header">
+			<h3>eClinic - Patients Manager</h3>
+		</div>
+
+		<div id="data">
+
+			<div class="tables">
+				<table>
+					<tr class="table_header">
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Date of Birth</th>
+						<th>Email Address</th>
+						<th>Phone Number</th>
+						<th>Street</th>
+						<th>City</th>
+						<th>Postal Code</th>
+						<th>File ID</th>
+						<th>Doctor ID</th>
+						<th>First Visit</th>
+						<th>Last Visit</th>
+						<th>Insurance</th>
+					</tr>
+
+					<c:forEach var="thePatient" items="${ allPatients }">
+
+						<tr>
+							<td>${ thePatient.firstName }</td>
+							<td>${ thePatient.lastName }</td>
+							<td>${ thePatient.dateOfBirth }</td>
+							<td>${ thePatient.emailAddress }</td>
+							<td>${ thePatient.phoneNumber }</td>
+							<td>${ thePatient.streetName }</td>
+							<td>${ thePatient.cityName }</td>
+							<td>${ thePatient.postalCode }</td>
+							<td>${ thePatient.fileId }</td>
+							<td>${ thePatient.doctorId }</td>
+							<td>${ thePatient.firstVisitDate }</td>
+							<td>${ thePatient.lastVisitDate }</td>
+							<td>${ thePatient.isInsurance }</td>
+						</tr>
+
+					</c:forEach>
+				</table>
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
